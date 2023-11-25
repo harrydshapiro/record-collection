3fc7a42ff386644eafc5f541740d00f34e9e2424
Harry Shapiro <harrydshapiro96@gmail.com>
2023-11-24 20:00:24 -0500

    add r stuff for sarah to connect to db

---
+++ data-analysis/connect.r
@@ -0,0 +1,15 @@
+library(DBI)
+library(RPostgres)
+con <- dbConnect(RPostgres::Postgres(),dbname = '', 
+                 host = '',
+                 port = 5432,
+                 user = '',
+                 password = '')
+
+dbListTables(con)
+
+res <- dbSendQuery(con, "SELECT * FROM users")
+dbFetch(res)
+dbClearResult(res)
+
+dbDisconnect(con)
