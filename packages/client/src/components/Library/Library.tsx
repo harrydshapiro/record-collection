c415e85d961bf3c9253b003db25cdf58eec24fbf
Harry Shapiro <50972797+harrydshapiro@users.noreply.github.com>
2024-11-17 14:59:59 -0500

    build fixed

--- packages/client/src/components/Library/Library.tsx
+++ packages/client/src/components/Library/Library.tsx
@@ -10,26 +10,30 @@ type LibraryProps = {
 };
 
 export function Library({ albums, onAlbumSelect }: LibraryProps) {
-  const sortByArtistThenAlbum = (a: GetAlbumsReturnType[0], b: GetAlbumsReturnType[0]) => (a.albumArtist === b.albumArtist 
-    ? a.albumName > b.albumName 
-        ? 1 : -1 
-    : a.albumArtist > b.albumArtist 
-        ? 1 : -1)
+  const sortByArtistThenAlbum = (
+    a: GetAlbumsReturnType[0],
+    b: GetAlbumsReturnType[0],
+  ) =>
+    a.albumArtist === b.albumArtist
+      ? a.albumName > b.albumName
+        ? 1
+        : -1
+      : a.albumArtist > b.albumArtist
+        ? 1
+        : -1;
   return (
     <div className={styles.libraryContainer}>
-      {albums
-        .sort(sortByArtistThenAlbum)
-        .map((a, index) => (
-          <div className={styles.albumCoverWrapper} key={index}>
-            <AlbumCover
-              onClick={onAlbumSelect}
-              albumId={a.albumId}
-              artistName={a.albumArtist}
-              albumName={a.albumName}
-              albumArtUrl={a.albumCoverArtUrl}
-            />
-          </div>
-        ))}
+      {albums.sort(sortByArtistThenAlbum).map((a, index) => (
+        <div className={styles.albumCoverWrapper} key={index}>
+          <AlbumCover
+            onClick={onAlbumSelect}
+            albumId={a.albumId}
+            artistName={a.albumArtist}
+            albumName={a.albumName}
+            albumArtUrl={a.albumCoverArtUrl}
+          />
+        </div>
+      ))}
     </div>
   );
 }
